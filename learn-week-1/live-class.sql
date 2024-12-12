4f264a4768ea97dc7f08dac7465f37243dec0494
althafdemiandra <althafdemiandraakbar@gmail.com>
2024-12-12 20:42:41 +0700

    chore: cae 2

--- learn-week-1/live-class.sql
+++ learn-week-1/live-class.sql
@@ -44,7 +44,8 @@ WITH
             ((msrp * 0.9) * quantityinstock) AS revenue
         FROM
             products
-    )
+    );
+
 SELECT
     productname,
     quantityinstock,
@@ -57,4 +58,40 @@ FROM
 ORDER BY
     profit DESC
 LIMIT
-    10
+    10;
+
+-- Case 2.1
+SELECT
+    *
+FROM
+    customers
+WHERE
+    customername ILIKE '%inc.%';
+
+-- Case 2.2
+SELECT
+    *
+FROM
+    customers
+WHERE
+    city IN ('San Francisco', 'NYC', 'Brickhaven')
+    OR country = 'Japan';
+
+-- Case 2.3
+SELECT
+    *
+FROM
+    customers
+WHERE
+    country = 'USA'
+    AND creditlimit > 0;
+
+-- Case 2.4
+SELECT
+    *
+FROM
+    payments
+ORDER BY
+    amount DESC
+LIMIT
+    10;
